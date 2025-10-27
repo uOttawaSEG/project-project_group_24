26ebb25b1b40974f90b6ec3be156b9f0045f1989
TwilightAspect <aamoy013@uottawa.ca>
2025-10-27 18:35:33 -0400

    Updated Added User Home UI

---
+++ app/src/main/java/com/uottawa/eecs/project_project_group_24/UserHomeActivity.java
@@ -0,0 +1,19 @@
+package com.uottawa.eecs.project_project_group_24;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class UserHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home);
+    }
+}
