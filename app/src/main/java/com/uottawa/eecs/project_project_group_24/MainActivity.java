754d62e1d002c059c9785f37aee4ba0dc69ee5e7
Abdou9810 <abdoussalam777@gmail.com>
2025-11-10 21:21:08 -0500

    cleaned up MainActivity

--- app/src/main/java/com/uottawa/eecs/project_project_group_24/MainActivity.java
+++ app/src/main/java/com/uottawa/eecs/project_project_group_24/MainActivity.java
@@ -22,71 +22,33 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnGeneral;
-    private FirebaseFirestore db;
-    DatabaseReference user_database;
+    private Button btnRegister, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        user_database = FirebaseDatabase.getInstance().getReference();
         FirebaseManager.getInstance().initializeRequests();
 
-        Button btnLogin = findViewById(R.id.btnLoginMain);
+        btnLogin = findViewById(R.id.btnLoginMain);
+        btnRegister = findViewById(R.id.btnGeneralRegister);
+
         btnLogin.setOnClickListener(v -> {
             Intent i = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(i);
         });
-//        Button btnAdmin = findViewById(R.id.adminButton);
-//        btnAdmin.setOnClickListener(v -> {
-//            Intent i = new Intent(MainActivity.this, AdminHomeActivity.class);
-//            startActivity(i);
-//        });
 
-
-//        btnStudent = findViewById(R.id.btnStudent);
-//        btnTutor = findViewById(R.id.btnTutor);
-        btnGeneral = findViewById(R.id.btnGeneralRegister);
-
-//        btnStudent.setOnClickListener(v -> {
-//            Intent i = new Intent(MainActivity.this, RegisterActivity.class);
-//            startActivity(i);
-//        });
-
-//        btnTutor.setOnClickListener(v -> {
-//            Intent i = new Intent(MainActivity.this, TutorRegisterActivity.class);
-//            startActivity(i);
-//        });
-		
-//		btnAdmin = findViewById(R.id.btnAdmin);
-//        btnAdmin.setOnClickListener(v -> {
-//            Intent i = new Intent(MainActivity.this, WelcomeActivity.class);
-//            i.putExtra("role", "administrator");
-//            startActivity(i);
-//        });
-
-        btnGeneral.setOnClickListener(v -> {
+        btnRegister.setOnClickListener(v -> {
             Intent i = new Intent(MainActivity.this, RegisterActivity.class);
             startActivity(i);
         });
-
-        // firebase
-
 
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
-    }
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-
     }
 
 }
